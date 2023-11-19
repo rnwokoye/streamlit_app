926cfb528ddca638b98e53468cd6b9321de81b79
Richard Uba Nwokoye <33564053+rnwokoye@users.noreply.github.com>
2023-11-19 01:47:58 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,8 +18,14 @@ my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/da
 
 
 # Fruit list here
-st.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+fruits_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 
-# Display the thable on the page
-st.dataframe(my_fruit_list)
+# fruits to show
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+
+# Display the table on the page
+# st.dataframe(my_fruit_list)
+st.dataframe(fruits_to_show)
+
+
 
