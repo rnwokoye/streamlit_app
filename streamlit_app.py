5570a2d5e72a0c4661d93bc8840c20677726fe52
Richard Nwokoye <RNwokoye@gmail.com>
2023-11-22 22:50:56 -0500

    added fruit choice input

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,3 +51,7 @@ st.header("Fruitvice Fruit Advice!")
 fruit_choice = st.text_input("What fruit would you like information about?", "Kiwi")
 st.write("The user entered", fruit_choice)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+
+# display user's choice in DF
+user_fruitchoice = pd.json_normalize(fruityvice_response.json())
+st.dataframe(user_fruitchoice)
