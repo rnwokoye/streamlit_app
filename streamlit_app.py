ec6c59de223e17e979dd6664888b331fd03c9a63
Richard Nwokoye <RNwokoye@gmail.com>
2023-11-23 17:18:50 -0500

    added snowflake connector

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import requests
+import snowflake.connector
 
 st.title("New Healthy Diner")
 
@@ -56,3 +57,13 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 # display user's choice in DF
 user_fruitchoice = pd.json_normalize(fruityvice_response.json())
 st.dataframe(user_fruitchoice)
+
+
+# Test our new snowflake.connector
+
+my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+st.text("Hello from Snowflake:")
+st.text(my_data_row)
